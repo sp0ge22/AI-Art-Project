fe0030b9283cde3b01f938a407857ed36f3ffc88
sp0ge22 <info@shinespikesolutions.com>
2023-06-06 14:20:47 -0400

    Updated photos

--- script.js
+++ script.js
@@ -18,9 +18,28 @@ tryAgainMessage.textContent = "Try again.";
 tryAgainMessage.classList.add("try-again-message");
 
 const images = [
-  { src: 'ai-generated-image-1.png', answer: ['surfing', 'monkey'], title: 'Kowabunga George', hints: ['Water skateboarding', 'Lil furry human'] },
-  { src: 'ai-generated-image-2.png', answer: ['skunk', 'soup'], title: 'Essence of Pew', hints: ['Smelly boy', 'sluuuuurppp'] },
-  { src: 'ai-generated-image-3.png', answer: ['porkchop', 'sandwiches'], title: 'The GI Special', hints: ['get the fuck outta here', 'you stupid idiot'] },
+  { src: 'ai-generated-image-1.png', answer: ['homeless', 'catfish'], title: 'Vagrant Mutant (by Rod)', hints: ['Vagabond', 'Tricked on tinder'] },
+  { src: 'ai-generated-image-2.png', answer: ['keyboard', 'warrior'], title: 'League of Legends (by Spoge)', hints: ['Clickty Clackity', 'Come out and playyy'] },
+  { src: 'ai-generated-image-3.png', answer: ['hockey', 'penguin'], title: 'Mighty Ducks (by Rod)', hints: ['Slap Shot', 'Ice Bird'] },
+  { src: 'ai-generated-image-4.png', answer: ['icecube', 'bacon'], title: 'No thanks (by Spoge)', hints: ['Cool ya drink', 'Salty Meat'] },
+  { src: 'ai-generated-image-5.png', answer: ['fish', 'car'], title: 'Bi-Modal Whip (by Spoge)', hints: ['Smells like your girl', 'Vroom Vroom'] },
+  { src: 'ai-generated-image-6.png', answer: ['pineapple', 'landline'], title: 'Sweet 70s iPhone (by Rod)', hints: ['Spongebob Squarepants', 'What your parents call it'] },
+  { src: 'ai-generated-image-7.png', answer: ['pork', 'police'], title: 'Stop you Swine (by Rod)', hints: ['Mmmm bacon', 'Blue Lives Matter'] },
+  { src: 'ai-generated-image-8.png', answer: ['minecraft', 'borat'], title: 'Notch Your Average Khazastanian (by Spoge)', hints: ['New Legos', 'Very nice!'] },
+  { src: 'ai-generated-image-9.png', answer: ['musical', 'chairs'], title: 'Rotating Vibes (by Spoge)', hints: ['Zack Efron', 'Take a seat'] },
+  { src: 'ai-generated-image-10.png', answer: ['tomato', 'elevator'], title: 'Growing Up (by Rod/Spoge)', hints: ['ketchup father', 'Hold your farts'] },
+  { src: 'ai-generated-image-11.png', answer: ['hat', 'house'], title: 'This one is good (by Rod)', hints: ['Yankee wit no brim', 'Domicile'] },
+  { src: 'ai-generated-image-12.png', answer: ['squirrell', 'astronaut'], title: 'Godspeed Squeakie (by Spoge)', hints: ['Yard rodent', 'Neil Armstronk'] },
+  { src: 'ai-generated-image-13.png', answer: ['alligator', 'chef'], title: 'Order Up (by Rod/Spoge)', hints: ['Florida mennace', 'Boppita Boopy'] },
+  { src: 'ai-generated-image-14.png', answer: ['stone', 'hinge'], title: 'Archaic Technology (by Spoge)', hints: ['Rocky boy', 'Eee Errr Eee Errr'] },
+  { src: 'ai-generated-image-15.png', answer: ['horse', 'playground'], title: 'Eqeustrian Recess (by Spoge)', hints: ['Pony Up Cowboy', 'Proving ground for kids'] },
+  { src: 'ai-generated-image-16.png', answer: ['quirky', 'corks'], title: 'Wine about it (by Spoge)', hints: ['Silly, funny, goofy', 'Wine Cap'] },
+  { src: 'ai-generated-image-17.png', answer: ['skatepark', 'church'], title: 'Shred the Sabbath (by Rod)', hints: ['Sick grinds my dude', 'Sunday Worship'] },
+  { src: 'ai-generated-image-18.png', answer: ['hamburger', 'headlamp'], title: 'Hearty Splunking (by Rod)', hints: ['Nom Nom Nom', 'Torch Hat'] },
+  { src: 'ai-generated-image-19.png', answer: ['samuari', 'vape'], title: 'Clouds of Tsushima (by Rod/Spoge', hints: ['Ninja warrior', 'Digital Dart'] },
+  { src: 'ai-generated-image-20.png', answer: ['broccoli', 'quarry'], title: 'Exscavating Nutrients (by Spoge)', hints: ['Rolling up dat', 'A rocky place'] },
+
+
   // add as many images as you want...
 ];
 
